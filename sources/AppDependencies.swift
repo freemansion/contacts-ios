0af27e04ba90329f7eab2c791580bc1e47710d23
Stanislau Baranouski <freemansion@gmail.com>
2019-02-02 14:34:36 +0700

    app dependencies managment

--- sources/AppDependencies.swift
+++ sources/AppDependencies.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+protocol HasAppConfig {
+    var appConfig: AppConfig { get }
+}
+
+protocol HasAppScreenManager {
+    var appScreenManager: AppScreenManager { get }
+}
+
+protocol HasNetworkDataProvider {
+    var dataProvider: NetworkDataProvider { get }
+}
+
+final class AppDependencies: HasAppConfig, HasAppScreenManager, HasNetworkDataProvider {
+    let appConfig: AppConfig
+    let appScreenManager: AppScreenManager
+    let dataProvider: NetworkDataProvider
+    
+    static let shared = AppDependencies()
+
+    init(appConfig: AppConfig = .default,
+         screenManager: AppScreenManager = .shared,
+         dataProvider: NetworkDataProvider = NetworkDataProvider()) {
+        self.appConfig = appConfig
+        self.appScreenManager = screenManager
+        self.dataProvider = dataProvider
+    }
+}
