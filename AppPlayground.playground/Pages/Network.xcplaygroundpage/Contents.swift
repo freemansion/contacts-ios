49e0cfebd4bb8130527937c567fd589666582cec
Stanislau Baranouski <freemansion@gmail.com>
2019-02-02 11:31:08 +0700

    added Network API usage example at the playground

--- AppPlayground.playground/Pages/Network.xcplaygroundpage/Contents.swift
+++ AppPlayground.playground/Pages/Network.xcplaygroundpage/Contents.swift
@@ -1,2 +1,22 @@
 import Foundation
-var str = "Hello, playground"
+import ContactModels
+import ContactsNetwork
+import Alamofire
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// In order to execute this Playground page make sure you've built `ContactModels` and `ContactsNetwork` frameworks
+// alternatively - you can select `contacts-ios` build scheme and press '⌘ B'
+
+let request = FetchContactsRequest()
+DataProvider.shared.request(.fetchAllContacts(request), source: .network) { (result: Result<[ContactsListPerson]>) in
+    switch result {
+    case .success(let contacts):
+        print(contacts.map { $0.fullName })
+    case .failure(let error):
+        print(error.localizedDescription)
+    }
+    PlaygroundPage.current.finishExecution()
+}
+
+
